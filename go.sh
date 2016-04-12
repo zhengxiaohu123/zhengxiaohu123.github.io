321160ef9d7438b40b1856492c0390785698f565
zhengxiaohu123 <461634936@qq.com>
2016-04-12 16:42:17 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "461634936@qq.com"
+git config --global user.name "zhengxiaohu123"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start http://zhengxiaohu123.github.io
